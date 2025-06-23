4757416695bf338b32e4c795a6cfd2cb6868b15b
mahmud <mahmudayaz2024@gmail.com>
2025-06-23 19:51:45 +0600

    init commit

---
+++ bin/test2.dart
@@ -0,0 +1,10 @@
+import 'untitled2.dart';
+
+main(){
+
+  Car car = Car();
+  car.setSpeed(80);
+  car.move();
+
+
+}
---
+++ bin/untitled2.dart
@@ -0,0 +1,37 @@
+abstract class Vehicle{
+
+  int _speed = 0;
+  
+  set setID(int value){
+    
+    _speed = value;
+  }
+
+  int get runingSpeed => _speed;
+  move();
+  setSpeed(int speed){
+    
+    setID = speed;
+
+  }
+
+}
+
+class Car extends Vehicle{
+  @override
+  move() {
+    print('The car is moving at $runingSpeed km/h');
+
+  }
+
+
+}
+
+main(){
+
+  Car car = Car();
+  car.setSpeed(80);
+  car.move();
+
+
+}
